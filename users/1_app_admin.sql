f61a933ad5f8317e6f0103b360fa4322fa58dcf8
Shreyas Purkar <purkarshreyas@gmail.com>
2024-11-04 19:02:18 -0500

    Add user app_admin

---
+++ users/1_app_admin.sql
@@ -0,0 +1,34 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    user_exists NUMBER;
+BEGIN
+    -- Check if the user exists
+    SELECT COUNT(*)
+    INTO user_exists
+    FROM all_users
+    WHERE username = 'APP_ADMIN';
+
+    -- Drop the user if it exists
+    IF user_exists > 0 THEN
+        EXECUTE IMMEDIATE 'DROP USER APP_ADMIN CASCADE';
+        DBMS_OUTPUT.PUT_LINE('User APP_ADMIN dropped successfully.');
+    END IF;
+    
+    -- Create the user with a password that contains special characters, enclosed in double quotes
+    EXECUTE IMMEDIATE 'CREATE USER APP_ADMIN IDENTIFIED BY "dbms#Admin1@ApiGateway"';
+    -- Grant basic system privileges
+    EXECUTE IMMEDIATE 'GRANT CONNECT TO APP_ADMIN';
+    -- Grant object creation privileges
+    EXECUTE IMMEDIATE 'GRANT CREATE VIEW, CREATE TABLE, CREATE SEQUENCE, CREATE SYNONYM TO APP_ADMIN';
+    -- Grant user management privileges
+    EXECUTE IMMEDIATE 'GRANT CREATE USER, ALTER USER, DROP USER TO APP_ADMIN';
+    -- Grant unlimited storage quota on the USERS tablespace
+    EXECUTE IMMEDIATE 'ALTER USER APP_ADMIN QUOTA UNLIMITED ON USERS';
+    DBMS_OUTPUT.PUT_LINE('User APP_ADMIN created and granted the specified privileges successfully.');
+    
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Something went wrong! Try again.');
+END;
+/
