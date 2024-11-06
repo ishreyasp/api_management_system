74dadbf0209952038867f2adebf04d78bf4c07da
iamneelabh <bhardwaj.ne@northeastern.edu>
2024-11-06 18:25:40 -0500

    Adding Veiw Billing_History

--- views/views.sql
+++ views/views.sql
@@ -12,3 +12,19 @@ CREATE OR REPLACE VIEW user_api_access AS
         JOIN api api ON api.api_id = api_access.api_id
     WHERE
         api_access.is_active = 1;
+        
+        
+-- View Billing History: billing history for each user, including subscription details.
+
+CREATE OR REPLACE VIEW billing_history AS
+    SELECT
+        b.billing_id,
+        b.billing_date,
+        b.total_amount,
+        s.*
+    FROM
+             subscription s
+        JOIN billing b ON s.subscription_id = b.subscription_id;
+
+
+
