244d44d02d148af558fb423de43c00dcdd356790
Deepthi-Nasika <nasika.d@northeastern.edu>
2024-12-01 21:30:28 -0500

    Procedure - Generate billing

---
+++ 3_procedures.sql
@@ -0,0 +1,413 @@
+-- Insert Packages
+CREATE OR REPLACE PACKAGE insert_into_api_management_system_pkg AS
+    PROCEDURE sp_subscribe_user_to_api (
+        p_username            IN api_users.username%TYPE,
+        p_api_id              IN api.api_id%TYPE,
+        p_pricing_model_id    IN pricing_model.model_id%TYPE,
+        p_start_date          IN subscription.start_date%TYPE,
+        p_end_date            IN subscription.end_date%TYPE,
+        p_message             OUT VARCHAR2
+    );
+    
+END insert_into_api_management_system_pkg;
+/
+
+CREATE OR REPLACE PACKAGE BODY insert_into_api_management_system_pkg AS
+
+    PROCEDURE sp_subscribe_user_to_api (
+        p_username            IN api_users.username%TYPE,
+        p_api_id              IN api.api_id%TYPE,
+        p_pricing_model_id    IN pricing_model.model_id%TYPE,
+        p_start_date          IN subscription.start_date%TYPE,
+        p_end_date            IN subscription.end_date%TYPE,
+        p_message             OUT VARCHAR2
+    )
+    AS
+    -- Variable declarations
+    v_usage_tracking_id usage_tracking.tracking_id%TYPE;
+    v_subscription_id   subscription.subscription_id%TYPE;
+    v_user_role         api_users.user_role%TYPe;
+    v_user_id           api_users.user_id%TYPE;
+    v_discount          subscription.discount%TYPE;
+    v_count             NUMBER;
+    
+    -- Custom exceptions
+    e_user_not_found            EXCEPTION;
+    e_api_not_found             EXCEPTION;
+    e_pricing_model_not_found   EXCEPTION;
+    BEGIN
+        -- Ensure that the user exists and is valid
+       IF NOT user_exists(p_username) THEN
+            RAISE e_user_not_found;
+        END IF;
+        
+        -- Get user id
+        v_user_id := get_user_id(p_username);
+        
+        -- Ensure that the API exists and is valid
+        IF NOT api_exists(p_api_id) THEN
+            RAISE e_api_not_found;
+        END IF;
+
+        -- Ensure that the pricing model exists and is valid
+        IF NOT pricing_model_exists(p_pricing_model_id, p_api_id) THEN
+            RAISE e_pricing_model_not_found;
+        END IF;
+    
+        -- Insert into Usage_Tracking table to create a tracking record for the user
+        INSERT INTO usage_tracking (
+            request_count, 
+            last_updated, 
+            limit_exceeded, 
+            api_id, 
+            user_id
+        ) VALUES (
+            0, 
+            SYSDATE, 
+            'N', 
+            p_api_id, 
+            v_user_id
+        )
+        RETURNING tracking_id INTO v_usage_tracking_id;
+        
+        -- If user role is Student then offer 20% discount
+        v_discount := calculate_discount_pct(p_username);
+
+        -- Insert into Subscription table
+        INSERT INTO subscription (
+            start_date, 
+            end_date, 
+            status, 
+            discount, 
+            user_id, 
+            pricing_model_id, 
+            usage_tracking_id
+        ) VALUES (
+            p_start_date, 
+            p_end_date, 
+            'Active', 
+            v_discount, 
+            v_user_id,  
+            p_pricing_model_id, 
+            v_usage_tracking_id
+        )
+        RETURNING subscription_id INTO v_subscription_id;
+
+        -- Commit the transaction
+        COMMIT;
+        
+        p_message := 'Subscription added successfully.';
+        
+    EXCEPTION
+        WHEN e_user_not_found THEN
+            p_message := 'User does not exist.';
+            ROLLBACK;
+            
+        WHEN e_api_not_found THEN
+            p_message := 'API does not exist.';
+            ROLLBACK;
+            
+        WHEN e_pricing_model_not_found THEN
+            p_message := 'Pricing model for the API does not exist.';
+            ROLLBACK;
+            
+        WHEN NO_DATA_FOUND THEN
+            p_message := 'API access or user record not found.';
+            ROLLBACK;
+
+        WHEN OTHERS THEN
+            p_message := 'An unexpected error occurred.';
+            ROLLBACK;
+    END sp_subscribe_user_to_api;
+
+END insert_into_api_management_system_pkg;     
+/
+
+CREATE OR REPLACE PACKAGE api_request_pkg AS  
+    PROCEDURE sp_api_request (
+        p_api_id        IN api.api_id%TYPE,
+        p_username      IN api_users.username%TYPE,
+        p_request_body  IN requests.request_body%TYPE,
+        p_status        IN requests.status%TYPE,
+        p_message       OUT VARCHAR2
+    );
+
+END api_request_pkg;
+/
+
+CREATE OR REPLACE PACKAGE BODY api_request_pkg AS
+
+    PROCEDURE sp_api_request (
+        p_api_id        IN api.api_id%TYPE,
+        p_username      IN api_users.username%TYPE,
+        p_request_body  IN requests.request_body%TYPE,
+        p_status        IN requests.status%TYPE,
+        p_message       OUT VARCHAR2
+    )
+    AS
+    -- Variable declarations
+    v_user_id               api_users.user_id%TYPE;
+    v_access_id             api_access.access_id%TYPE;
+    v_is_active             api_access.is_active%TYPE;
+    v_token_enddate         api_users.api_token_enddate%TYPE;
+    v_response_time         requests.response_time%TYPE;
+    v_is_limit_exceeded     usage_tracking.limit_exceeded%TYPE;
+    v_response_body         requests.response_body%TYPE;
+
+    -- Custom exceptions
+    e_token_expired           EXCEPTION;
+    e_no_api_access           EXCEPTION;
+    e_request_limit_exceeded  EXCEPTION;
+    e_user_not_found          EXCEPTION;
+    e_api_not_found           EXCEPTION;
+    
+    BEGIN
+    
+        -- Ensure that the user exists and is valid
+        IF NOT user_exists(p_username) THEN
+            RAISE e_user_not_found;
+        END IF;
+        
+        -- Get user id
+        v_user_id := get_user_id(p_username);
+        
+        -- Ensure that the API exists and is valid
+        IF NOT api_exists(p_api_id) THEN
+            RAISE e_api_not_found;
+        END IF;
+        
+        -- Fetch access_id, is_active and api_token_enddate
+        SELECT a.access_id, a.is_active, u.api_token_enddate
+        INTO v_access_id, v_is_active, v_token_enddate
+        FROM api_access a
+        JOIN api_users u ON u.user_id = v_user_id
+        WHERE a.api_id = p_api_id
+        AND a.user_id = v_user_id;
+       
+        -- Check if the API token has expired
+        IF v_token_enddate < SYSDATE THEN
+            RAISE e_token_expired;
+        END IF;
+        
+        -- Check if the API access is active
+        IF v_is_active = 'N' THEN
+            RAISE e_no_api_access;
+        END IF;
+        
+        -- Fetch limit_exceeded from usage_tracking table
+        BEGIN
+            SELECT limit_exceeded
+            INTO v_is_limit_exceeded
+            FROM usage_tracking
+            WHERE api_id = p_api_id
+            AND user_id = v_user_id;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                v_is_limit_exceeded := 'N'; 
+        END;
+        
+        -- Check if request limit is exceeded
+        IF UPPER(v_is_limit_exceeded) = 'Y' THEN
+            RAISE e_request_limit_exceeded;
+        END IF;
+
+        -- Generate a random response time between 0.1 and 1 second
+        v_response_time := ROUND(DBMS_RANDOM.VALUE(0.1, 1), 2);
+        
+        -- Generate random response body
+        v_response_body := generate_random_text(20);
+
+        -- Insert a record into the requests table and return the request_id
+        INSERT INTO requests (
+            response_time,
+            status,
+            request_body,
+            response_body,
+            access_id
+        ) VALUES (
+            v_response_time,
+            p_status,
+            p_request_body,
+            v_response_body,
+            v_access_id
+        )
+        RETURNING request_id INTO p_message;  
+
+        -- Commit the transaction
+        COMMIT;
+    
+        p_message := 'API request successful. Request ID: ' || p_message;
+
+    EXCEPTION
+        WHEN e_user_not_found THEN
+            p_message := 'User does not exist.';
+            ROLLBACK;
+            
+        WHEN e_api_not_found THEN
+            p_message := 'API does not exist.';
+            ROLLBACK;
+            
+        WHEN e_token_expired THEN
+            p_message := 'API access denied: Token has expired.';
+            ROLLBACK;
+
+        WHEN e_no_api_access THEN
+            p_message := 'API access denied: Access is inactive.';
+            ROLLBACK;
+
+        WHEN e_request_limit_exceeded THEN
+            p_message := 'API access denied: API request limit reached.';
+            ROLLBACK;   
+
+        WHEN NO_DATA_FOUND THEN
+            p_message := 'API access or user record not found.';
+            ROLLBACK;
+
+        WHEN OTHERS THEN
+            p_message := 'An unexpected error occurred: ' || SQLERRM;
+            ROLLBACK;
+            
+    END sp_api_request;
+
+END api_request_pkg;
+/
+
+
+-- Generate billing
+CREATE OR REPLACE PACKAGE billing_pkg AS
+    PROCEDURE generate_billing (
+        p_subscription_id    IN subscription.subscription_id%TYPE,
+        p_username           IN api_users.username%TYPE,
+        p_api_id             IN api.api_id%TYPE
+    );
+    
+END billing_pkg;
+/
+
+CREATE OR REPLACE PACKAGE BODY billing_pkg AS
+
+    PROCEDURE generate_billing (
+        p_subscription_id    IN subscription.subscription_id%TYPE,
+        p_username           IN api_users.username%TYPE,
+        p_api_id             IN api.api_id%TYPE
+    )
+    AS
+
+        v_total_amount      FLOAT := 0;
+        v_user_id           api_users.user_id%TYPE;
+        v_pricing_model_id  subscription.pricing_model_id%TYPE;
+        v_discount          subscription.discount%TYPE;
+        v_usage_tracking_id usage_tracking.tracking_id%TYPE;
+        v_request_count     usage_tracking.request_count%TYPE;
+        v_model_type        pricing_model.model_type%TYPE;
+        v_rate              pricing_model.rate%TYPE;
+        v_request_limit     pricing_model.request_limit%TYPE;
+        
+
+        -- Custom exceptions
+        e_subscription_not_found     EXCEPTION;
+        e_pricing_model_not_found    EXCEPTION;
+        e_user_not_found             EXCEPTION;
+        e_api_not_found              EXCEPTION;
+        e_request_count_mismatch     EXCEPTION;
+
+    BEGIN
+        -- Check if the user exists
+        IF NOT user_exists(p_username) THEN
+            RAISE e_user_not_found;
+        END IF;
+        
+        -- Get user id
+        v_user_id := get_user_id(p_username);
+
+        -- Ensure that the API exists and is valid
+        IF NOT api_exists(p_api_id) THEN
+            RAISE e_api_not_found;
+        END IF;
+
+        -- Check if the subscription exists
+        IF NOT subscription_exists(p_subscription_id) THEN
+            RAISE e_subscription_not_found;
+        END IF;
+
+        -- Retrieve pricing model ID, tracking id, discount for the subscription
+        SELECT pricing_model_id, NVL(discount, 0), tracking_id
+        INTO v_pricing_model_id, v_discount, v_usage_tracking_id
+        FROM subscription
+        WHERE subscription_id = p_subscription_id;
+
+        -- Check if the pricing model exists
+        IF NOT pricing_model_exists(v_pricing_model_id, p_api_id) THEN
+            RAISE e_pricing_model_not_found;
+        END IF;
+
+        -- Retrieve model_type, request_limit, rate from pricing_model
+        SELECT model_type, rate, request_limit
+        INTO v_model_type, v_rate, v_request_limit
+        FROM pricing_model 
+        WHERE model_id = v_pricing_model_id;
+
+        -- Retrieve request_count from usage_tracking
+        SELECT request_count
+        INTO v_request_count
+        FROM usage_tracking
+        WHERE tracking_id = v_usage_tracking_id;
+
+        -- Check if the request limit matches the request count for "Subscription" model
+        IF v_model_type = 'Subscription' AND v_request_limit <> v_request_count THEN
+            RAISE e_request_count_mismatch;
+        END IF;
+
+        -- Calculate the total amount for Subscription model type
+        IF v_model_type = 'Subscription' THEN
+            v_total_amount := v_rate * v_request_limit;
+
+        -- Calculate the total amount for Pay_Per_Usage model type
+        IF v_model_type = 'Pay_Per_Usage' THEN
+            v_total_amount := v_rate * v_request_count;
+
+        -- Insert the billing record into the billing table
+        INSERT INTO billing (
+            billing_date,
+            total_amount,
+            subscription_id
+        ) VALUES (
+            SYSDATE,
+            v_total_amount,
+            p_subscription_id
+        );
+
+        -- Commit the transaction
+        COMMIT;
+
+        -- Success message
+        p_message := 'Billing record generated successfully with total amount: ' || v_total_amount;
+
+    EXCEPTION
+        WHEN e_user_not_found THEN
+            p_message := 'User does not exist.';
+            ROLLBACK;
+
+        WHEN e_api_not_found THEN
+            p_message := 'API does not exist.';
+            ROLLBACK;
+
+        WHEN e_subscription_not_found THEN
+            p_message := 'Subscription does not exist.';
+            ROLLBACK;
+
+        WHEN e_pricing_model_not_found THEN
+            p_message := 'Pricing model does not exist.';
+            ROLLBACK;
+
+        WHEN e_request_count_mismatch THEN
+            p_message := 'Request count does not match request limit for subscription model.';
+            ROLLBACK;
+
+        WHEN OTHERS THEN
+            p_message := 'An unexpected error occurred: ' || SQLERRM;
+            ROLLBACK;
+    END generate_billing;
+
+END billing_pkg;
+/
---
+++ 4_functions.sql
@@ -0,0 +1,313 @@
+-- Function to check if given user is valid
+CREATE OR REPLACE FUNCTION user_exists (
+    p_username IN api_users.username%TYPE
+) 
+RETURN BOOLEAN 
+AS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM api_users
+    WHERE username = p_username;
+
+    IF v_count = 0 THEN
+        RETURN FALSE;  
+    ELSE
+        RETURN TRUE;   
+    END IF;
+END user_exists;
+/
+
+-- Function to get the user id for given username
+CREATE OR REPLACE FUNCTION get_user_id (
+    p_username IN api_users.username%TYPE
+) 
+RETURN api_users.user_id%TYPE 
+AS
+    v_user_id api_users.user_id%TYPE;
+BEGIN
+    SELECT user_id
+    INTO v_user_id
+    FROM api_users
+    WHERE username = p_username;
+
+    RETURN v_user_id;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20000, 'User does not exist.');
+END get_user_id;
+/
+
+-- Function to check if api with given api id exists
+CREATE OR REPLACE FUNCTION api_exists (
+    p_api_id IN api.api_id%TYPE
+) 
+RETURN BOOLEAN 
+AS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM api
+    WHERE api_id = p_api_id;
+
+    IF v_count = 0 THEN
+        RETURN FALSE;  
+    ELSE
+        RETURN TRUE;   
+    END IF;
+END api_exists;
+/
+
+-- Check if pricing model exists
+CREATE OR REPLACE FUNCTION pricing_model_exists (
+    p_model_id IN pricing_model.model_id%TYPE,
+    p_api_id   IN api.api_id%TYPE
+) 
+RETURN BOOLEAN 
+AS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM pricing_model
+    WHERE model_id = p_model_id AND api_id = p_api_id;
+
+    IF v_count = 0 THEN
+        RETURN FALSE;  
+    ELSE
+        RETURN TRUE;   
+    END IF;
+END pricing_model_exists;
+/
+
+-- Function to check if subscription exists
+CREATE OR REPLACE FUNCTION subscription_exists (
+    p_subscription_id IN subscription.subscription_id%TYPE
+) 
+RETURN BOOLEAN 
+AS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM subscription
+    WHERE subscription_id = p_subscription_id;
+
+    IF v_count = 0 THEN
+        RETURN FALSE;  
+    ELSE
+        RETURN TRUE;   
+    END IF;
+END subscription_exists;
+/
+
+-- Function to calculate the subscription discount for given username 
+CREATE OR REPLACE FUNCTION calculate_discount_pct (p_username IN api_users.username%TYPE) 
+RETURN NUMBER 
+AS
+    v_discount  NUMBER(5, 3);
+    v_user_role VARCHAR2(10);
+BEGIN
+    
+    SELECT user_role
+    INTO v_user_role
+    FROM api_users
+    WHERE username = p_username;
+        
+    IF v_user_role = 'Student' THEN
+        v_discount := 0.20;
+    ELSE 
+        v_discount := 0.00;    
+    END IF;
+    
+    RETURN v_discount;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Error calculating discount: ' || SQLERRM);
+END calculate_discount_pct;
+/
+
+-- Function to check if the user has an active subscription for the given API
+CREATE OR REPLACE FUNCTION is_active_subscription (
+    p_user_id    IN api_users.user_id%TYPE,
+    p_api_id     IN api.api_id%TYPE,
+    p_model_id   IN pricing_model.model_id%TYPE 
+) RETURN BOOLEAN
+AS
+    v_subscription_status subscription.status%TYPE;
+    v_count INTEGER;
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM api_access
+    WHERE api_id = p_api_id
+      AND user_id = p_user_id;
+
+    IF v_count = 0 THEN
+        RETURN FALSE;  
+    END IF;
+
+    BEGIN
+        SELECT s.status
+        INTO v_subscription_status
+        FROM subscription s
+        JOIN api_access a ON s.user_id = a.user_id
+        WHERE s.user_id = p_user_id
+          AND a.api_id = p_api_id
+          AND s.pricing_model_id = p_model_id
+          AND s.status = 'Active';
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN FALSE;  
+    END;
+
+    RETURN TRUE;  
+END is_active_subscription;
+/
+
+-- Function to generate random text
+CREATE OR REPLACE FUNCTION generate_random_text(
+    p_length IN NUMBER
+) RETURN VARCHAR2 
+AS
+    v_random_text VARCHAR2(4000);
+    v_characters  CONSTANT VARCHAR2(62) := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    v_characters_length CONSTANT NUMBER := LENGTH(v_characters);
+    v_index NUMBER;
+BEGIN
+    v_random_text := '';
+    FOR i IN 1..p_length LOOP
+        v_index := ROUND(DBMS_RANDOM.VALUE(1, v_characters_length));
+        v_random_text := v_random_text || SUBSTR(v_characters, v_index, 1);
+    END LOOP;
+    RETURN v_random_text;
+    
+END generate_random_text;
+/
+
+-- Function to get user engagement report
+CREATE OR REPLACE FUNCTION get_user_engagement_report
+RETURN SYS_REFCURSOR
+AS
+    user_report SYS_REFCURSOR;
+BEGIN
+    OPEN user_report FOR
+        WITH user_activity AS (
+            SELECT 
+                u.username,
+                COUNT(r.request_id) AS total_requests,
+                MAX(r.req_timestamp) AS last_request_date,
+                CASE 
+                    WHEN MAX(r.req_timestamp) < SYSDATE - 90 THEN 'Inactive'
+                    ELSE 'Active'
+                END AS user_status
+            FROM 
+                api_users u
+            JOIN 
+                api_access ac ON u.user_id = ac.user_id
+            JOIN 
+                requests r ON ac.access_id = r.access_id
+            GROUP BY 
+                u.username
+        ),
+        user_segments AS (
+            SELECT 
+                username,
+                total_requests,
+                user_status,
+                NTILE(3) OVER (ORDER BY total_requests DESC) AS engagement_tier_rank
+            FROM 
+                user_activity
+        )
+        SELECT 
+            username AS "Username",
+            user_status AS "Status",
+            CASE 
+                WHEN engagement_tier_rank = 1 THEN 'High Engagement'
+                WHEN engagement_tier_rank = 2 THEN 'Medium Engagement'
+                ELSE 'Low Engagement'
+            END AS "Engagement Tier",
+            total_requests AS "Total Requests"
+        FROM 
+            user_segments
+        ORDER BY 
+            engagement_tier_rank, total_requests DESC;
+
+    RETURN user_report;
+END get_user_engagement_report;
+/
+
+-- Function to get API revenue data
+CREATE OR REPLACE FUNCTION get_api_revenue_data
+RETURN SYS_REFCURSOR
+AS
+    revenue_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN revenue_cursor FOR
+    WITH revenue_data AS (
+        SELECT 
+            a.name AS api_name,
+            pm.model_type AS pricing_model,
+            COUNT(r.request_id) AS total_requests,
+            SUM(CASE WHEN r.status = 'SUCCESS' THEN 1 ELSE 0 END) AS success_requests,
+            SUM(CASE WHEN r.status = 'FAILED' THEN 1 ELSE 0 END) AS failed_requests,
+            COUNT(CASE WHEN r.status = 'SUCCESS' THEN 1 END) * pm.rate AS total_revenue_generated,
+            COUNT(CASE WHEN r.status = 'FAILED' THEN 1 END) * pm.rate AS total_revenue_loss
+        FROM 
+            api a
+        JOIN 
+            api_access ac ON a.api_id = ac.api_id
+        JOIN 
+            subscription s ON ac.user_id = s.user_id
+        JOIN 
+            pricing_model pm ON s.pricing_model_id = pm.model_id
+        LEFT JOIN 
+            requests r ON ac.access_id = r.access_id
+        GROUP BY 
+            a.name, pm.model_type, pm.rate
+    )
+    SELECT 
+        api_name AS "API Name",
+        pricing_model AS "Pricing Model",
+        success_requests AS "Successful Requests",
+        failed_requests AS "Failed Requests",
+        total_revenue_generated AS "Revenue Generated",
+        total_revenue_loss AS "Revenue Lost"
+    FROM 
+        revenue_data
+    ORDER BY 
+        total_requests DESC;
+
+    RETURN revenue_cursor;
+END;
+/
+
+-- Function to get API response time
+CREATE OR REPLACE FUNCTION get_api_response_time_report
+RETURN SYS_REFCURSOR
+AS
+    response_time_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN response_time_cursor FOR
+    SELECT  
+        a.name AS "API Name",
+        TO_CHAR(r.req_timestamp, 'YYYY-MM') AS "Month",
+        ROUND(AVG(r.response_time), 2) AS "Average Response Time (s)",
+        MIN(r.response_time) AS "Minimum Response Time (s)",
+        MAX(r.response_time) AS "Maximum Response Time (s)"
+    FROM 
+        requests r
+    JOIN 
+        api_access ac ON r.access_id = ac.access_id
+    JOIN 
+        api a ON ac.api_id = a.api_id
+    GROUP BY 
+        a.name, TO_CHAR(r.req_timestamp, 'YYYY-MM')
+    ORDER BY 
+        "Month", "Average Response Time (s)" DESC;
+
+    RETURN response_time_cursor;
+END;
+/
